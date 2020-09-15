1e8e104e224fadfa0b922334dcbd13030251ad73
Cornel-Adrian <co96grig@gmail.com>
2020-09-15 23:39:43 +0300

    Front end Update v1.5

--- middlewares/index.js
+++ middlewares/index.js
@@ -6,7 +6,7 @@ module.exports = function (app, express) {
     app.use(Sentry.Handlers.requestHandler());
     app.use(Sentry.Handlers.errorHandler());
     app.set('view engine', 'ejs');
-    app.use('/static', express.static('public'));
+    app.use(express.static('public'));
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(express.json());
 
