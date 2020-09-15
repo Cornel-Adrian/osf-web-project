910979883ae5fe5b370365d8e7a59aca72b58761
Cornel-Adrian <co96grig@gmail.com>
2020-09-15 13:39:33 +0300

    Front End Started Somehow

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const Sentry = require("@sentry/node");
 const PORT = process.env.PORT || 3000;
 const SENTRYDSN = process.env.SENTRYDSN;
@@ -6,30 +7,18 @@ const SENTRYDSN = process.env.SENTRYDSN;
 require('./routes')(app);
 require('dotenv').config()
 
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
 
 Sentry.init({
   dsn: SENTRYDSN,
   tracesSampleRate: 1.0,
 });
 
-const transaction = Sentry.startTransaction({
-  op: "test",
-  name: "My First Test Transaction",
-});
-
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${PORT}`);
 });
 
-app.set('view engine', 'ejs');
 
-setTimeout(() => {
-  try {
-    // foo();
-  } catch (e) {
-    Sentry.captureException(e);
-  } finally {
-    transaction.finish();
-  }
-}, 99);
+
 module.exports = app;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,4 @@
 module.exports = function (app) {
 
     app.use(Sentry.Handlers.errorHandler());
-    // app.set('views', path.join(__dirname, 'views'));
-    // app.set('view engine', 'ejs');
 }
